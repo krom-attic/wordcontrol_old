df3ed5be62806fa3b0e20e43f9d10762c2df5830
Blane <blanebf@gmail.com>
2013-09-29 13:25:14 +0400

    Added global abstract models and dictionary term models

--- wordengine/models.py
+++ wordengine/models.py
@@ -1,3 +1,90 @@
 from django.db import models
+from django.contrib import auth
 
-# Create your models here.
+# Global abstract classes
+
+
+class Change(models.Model):
+    """Abstract base class representing submitted change."""
+
+    user_changer = models.ForeignKey(auth.models.User, editable=False, related_name="%(app_label)s_%(class)s_changer")
+    timestamp_change = models.DateTimeField(auto_now_add=True, editable=False)
+    comment = models.TextField()
+
+    class Meta:
+        abstract = True
+
+
+class MiscChange(Change):
+    table_name = models.CharField()
+    field_name = models.CharField()
+    old_value = models.CharField()
+    new_value = models.CharField()
+
+
+# Dictionary term models
+
+
+class Term(models.Model):
+    """Abstract base class for all terms in dictionary."""
+
+    term_full = models.CharField()
+    term_abbr = models.CharField()
+
+    class Meta:
+        abstract = True
+
+
+class SyntacticCategory(Term):
+    pass
+
+
+class UsageConstraints(Term):
+    pass
+
+
+class Animacy(Term):
+    pass
+
+
+class Aspect(Term):
+    pass
+
+
+class Case(Term):
+    pass
+
+
+class Comparison(Term):
+    pass
+
+
+class Gender(Term):
+    pass
+
+
+class Mood(Term):
+    pass
+
+
+class Number(Term):
+    pass
+
+
+class Person(Term):
+    pass
+
+
+class Polarity(Term):
+    pass
+
+
+class Tense(Term):
+    pass
+
+
+class Voice(Term):
+    pass
+
+
+
