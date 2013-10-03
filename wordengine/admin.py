e7f985ddfe2aa8e81e55847f956d745f8287687d
Blane <blanebf@gmail.com>
2013-10-03 23:34:03 +0400

    Added models from wordengine to admin application

---
+++ wordengine/admin.py
@@ -0,0 +1,28 @@
+__author__ = 'Blane'
+
+from django.contrib import admin
+import wordengine.models
+
+admin.site.register(wordengine.models.SyntacticCategory)
+admin.site.register(wordengine.models.UsageConstraint)
+admin.site.register(wordengine.models.Animacy)
+admin.site.register(wordengine.models.Aspect)
+admin.site.register(wordengine.models.Case)
+admin.site.register(wordengine.models.Comparison)
+admin.site.register(wordengine.models.Gender)
+admin.site.register(wordengine.models.Mood)
+admin.site.register(wordengine.models.Number)
+admin.site.register(wordengine.models.Person)
+admin.site.register(wordengine.models.Polarity)
+admin.site.register(wordengine.models.Tense)
+admin.site.register(wordengine.models.Voice)
+admin.site.register(wordengine.models.GrammarCategorySet)
+admin.site.register(wordengine.models.Language)
+admin.site.register(wordengine.models.Dialect)
+admin.site.register(wordengine.models.WritingSystem)
+admin.site.register(wordengine.models.Lexeme)
+admin.site.register(wordengine.models.WordForm)
+admin.site.register(wordengine.models.Translation)
+admin.site.register(wordengine.models.TranslatedTerm)
+
+
