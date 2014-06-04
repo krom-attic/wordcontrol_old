138956db16daf81f1d0396bf3ef21138fef1fbcd
Kirill Grushetsky <krommail@gmail.com>
2014-06-04 17:58:59 +0400

    South installed

--- dj_wordcontrol/settings.py
+++ dj_wordcontrol/settings.py
@@ -129,7 +129,12 @@ INSTALLED_APPS = (
     'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
-    'wordengine'
+
+    # HOMEBREW APPS
+    'wordengine',
+
+    # THIRDPARTY APPS
+    'south',
 )
 
 # A sample logging configuration. The only tangible logging
