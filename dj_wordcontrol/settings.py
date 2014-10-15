07f434ef658c481ced24c30e0ff3e7e5c9a8d724
Krom <krommail@gmail.com>
2014-10-16 00:51:22 +0400

    Testmode

--- dj_wordcontrol/settings.py
+++ dj_wordcontrol/settings.py
@@ -18,7 +18,8 @@ MANAGERS = ADMINS
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',  # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': os.path.join(PROJECT_DIR, 'sqlite3.db'),  # Or path to database file if using sqlite3.
+        # 'NAME': os.path.join(PROJECT_DIR, 'sqlite3.db'),  # Or path to database file if using sqlite3.
+        'NAME': os.path.join(PROJECT_DIR, 'sqlite3.db'),  # Database for testing
         # The following settings are not used with sqlite3:
         'USER': '',
         'PASSWORD': '',
--- wordengine/forms.py
+++ wordengine/forms.py
@@ -80,7 +80,7 @@ class ProjectColumnSetupForm(forms.ModelForm):
 
     class Meta:
         model = models.ProjectColumn
-        widgets = {'project': forms.HiddenInput, 'state': forms.HiddenInput, 'literal': forms.HiddenInput}
+        widgets = {'project': forms.MultipleHiddenInput, 'state': forms.HiddenInput, 'literal': forms.HiddenInput}
         # exclude = ['literal', 'project', 'state']
 
 
