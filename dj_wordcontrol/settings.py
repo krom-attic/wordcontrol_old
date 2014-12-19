2c742c61e301ffff54916fa62077aa5461d86ead
Krom <krommail@gmail.com>
2014-12-19 23:27:55 +0300

    deploy fix

--- dj_wordcontrol/settings.py
+++ dj_wordcontrol/settings.py
@@ -69,7 +69,10 @@ MEDIA_URL = ''
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/var/www/example.com/static/"
-STATIC_ROOT = ''
+if DEBUG:
+    STATIC_ROOT = ''
+else:
+    STATIC_ROOT = '/www/site1/webapp/wordcontrol/'
 
 # URL prefix for static files.
 # Example: "http://example.com/static/", "http://static.example.com/"
